84a67fd72e733e81e8fa5a1002865801093f97f2
Armando Ugalde <armauv@gmail.com>
2021-07-08 21:04:44 -0500

    Increase Flake line length

--- migrations/env.py
+++ migrations/env.py
@@ -21,10 +21,10 @@ logger = logging.getLogger("alembic.env")
 # target_metadata = mymodel.Base.metadata
 config.set_main_option(
     "sqlalchemy.url",
-    str(current_app.extensions["migrate"]
-        .db.get_engine().url).replace("%", "%%"),
+    str(current_app.extensions["migrate"].db.get_engine().url).replace("%", "%%"),
 )
 target_metadata = current_app.extensions["migrate"].db.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -45,9 +45,7 @@ def run_migrations_offline():
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url,
-                      target_metadata=target_metadata,
-                      literal_binds=True)
+    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
 
     with context.begin_transaction():
         context.run_migrations()

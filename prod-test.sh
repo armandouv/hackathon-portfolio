b7cb323a963d776048cc32a3408bd34162836f1a
Armando Ugalde <armauv@gmail.com>
2021-07-09 19:56:02 -0500

    Add TERM env variable

--- prod-test.sh
+++ prod-test.sh
@@ -2,48 +2,42 @@ BASE_URL=https://armandouv.duckdns.org/
 CURRENT_URL=$BASE_URL
 NEW_USERNAME=$(openssl rand -base64 12)
 NEW_PASSWORD=hola
-TERM="xterm"
 red=$(tput setaf 1)
 green=$(tput setaf 2)
 
 compare_status() {
-    if [ "$1" == "$2" ]
-    then
-        echo "$green""Passed"
-    else
-        echo "$red""Failed"
-        exit 1
-    fi
-    reset=$(tput sgr0)
-    echo "$reset"
+  if [ "$1" == "$2" ]; then
+    echo "$green""Passed"
+  else
+    echo "$red""Failed"
+    exit 1
+  fi
+  reset=$(tput sgr0)
+  echo "$reset"
 }
 
 expect_get_status() {
-    STATUS_CODE=$(curl -s --ignore-content --head $CURRENT_URL | head -1 | cut -d " " -f 2)
-    compare_status "$1" "$STATUS_CODE"
+  STATUS_CODE=$(curl -s --ignore-content --head $CURRENT_URL | head -1 | cut -d " " -f 2)
+  compare_status "$1" "$STATUS_CODE"
 }
 
 expect_post_status() {
-    STATUS_CODE=$(curl -w "%{http_code}" -X POST -d "$2" $CURRENT_URL -s -o /dev/null)
-    compare_status "$1" "$STATUS_CODE"
+  STATUS_CODE=$(curl -w "%{http_code}" -X POST -d "$2" $CURRENT_URL -s -o /dev/null)
+  compare_status "$1" "$STATUS_CODE"
 }
 
-print_url()
-{
-    echo
-    yellow=$(tput setaf 3)
-    echo "${yellow}""$1"
-    reset=$(tput sgr0)
-    echo "$reset"
+print_url() {
+  echo
+  yellow=$(tput setaf 3)
+  echo "${yellow}""$1"
+  reset=$(tput sgr0)
+  echo "$reset"
 }
-
-
 
 CURRENT_URL=$BASE_URL
 print_url /
 echo "Home page:"
 expect_get_status 200
-
 
 CURRENT_URL=${BASE_URL}gfsag
 print_url /gfsag

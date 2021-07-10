4059f7e0efac86379770a55d0eeb2d642007c350
Armando Ugalde <armauv@gmail.com>
2021-07-09 19:29:16 -0500

    Restore prod-test

--- prod-test.sh
+++ prod-test.sh
@@ -109,5 +109,3 @@ expect_post_status 418 "username=FGSFD&password=$NEW_PASSWORD"
 
 echo "Invalid user login (incorrect password):"
 expect_post_status 418 "username=$NEW_USERNAME&password=invalid"
-
-exit 1

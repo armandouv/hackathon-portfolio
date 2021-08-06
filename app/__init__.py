3aec673c93295986f0dd1837b02b1f20365d9b93
Carlos Moisés Chávez Jiménez <carlosmoiseschavezjimenez@gmail.com>
2021-08-05 20:56:04 -0500

    Modified python file

--- app/__init__.py
+++ app/__init__.py
@@ -1,55 +1,182 @@
+import os
+
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for, abort
+from flask.helpers import flash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import (
+    LoginManager,
+    login_user,
+    logout_user,
+    login_required,
+    current_user,
+)
+from .models import UserModel
+from data.load_data import load_projects, load_profiles
 
+load_dotenv()
 db = SQLAlchemy()
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = "t4{pt_+FS3T#G\Gfs/F"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sql"
-    """app.config[
-        "SQLALCHEMY_DATABASE_URI"
-    ] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
-        user=os.getenv("POSTGRES_USER"),
-        passwd=os.getenv("POSTGRES_PASSWORD"),
-        host=os.getenv("POSTGRES_HOST"),
-        port=5432,
-        table=os.getenv("POSTGRES_DB"),
-    )"""
+# def create_app():
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "t4{pt_+FS3T#G\Gfs/F"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sql"
+"""app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
+    port=5432,
+    table=os.getenv("POSTGRES_DB"),
+)"""
 
-    db.init_app(app)
+db.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = "auth.login"
-    login_manager.init_app(app)
-
-    from .models import UserModel
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
-        return UserModel.query.get(int(user_id))
-
-    # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
-
-    app.register_blueprint(auth_blueprint)
-
-    # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
-
-    app.register_blueprint(main_blueprint)
-
-    migrate = Migrate(app, db)
-
-    return app
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+login_manager.init_app(app)
 
 
-@app.route("/")
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return UserModel.query.get(int(user_id))
+
+
+# blueprint for auth routes in our app
+from .auth import auth as auth_blueprint
+
+app.register_blueprint(auth_blueprint)
+
+# blueprint for non-auth parts of app
+from .main import main as main_blueprint
+
+app.register_blueprint(main_blueprint)
+
+migrate = Migrate(app, db)
+
+
+base_url = os.getenv("URL")
+projects_base_url = base_url + "/projects/"
+profiles_base_url = base_url + "/profiles/"
+
+projects = load_projects()
+profiles = load_profiles()
+# return app
+
+
+@main.route("/")
 def index():
     return render_template("index.html", title="Team Kenargi's portfolio")
+
+
+@app.route("/profile")
+@login_required
+def profile():
+    return render_template("profile.html", name=current_user.name)
+
+
+@app.route("/projects/<name>")
+def get_project(name):
+    if name not in projects:
+        return abort(404)
+    return render_template(
+        "project.html", item=projects[name], title=name, url=projects_base_url + name
+    )
+
+
+@app.route("/profiles/<name>")
+def get_profile(name):
+    if name not in profiles:
+        return abort(404)
+    title = name + "'s Profile"
+    return render_template(
+        "profile.html", item=profiles[name], title=title, url=profiles_base_url + name
+    )
+
+
+@app.route("/health")
+def get_health():
+    return "", 200
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html", title="Page not found"), 404
+
+
+@app.route("/addpost", methods=("GET", "POST"))
+def addpost():
+    if session["username"] in session:
+        return render_template("index.html", title="Home")
+    else:
+        return render_template("login.html", title="Login")
+
+
+@app.route("/register", methods=("GET", "POST"))
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        error = None
+
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+        else:
+            user = UserModel.query.filter_by(username=username).first()
+
+        if user:
+            flash("Email address already exists")
+            return redirect(url_for("auth.signup"))
+
+        if error is None:
+            new_user = UserModel(
+                username, generate_password_hash(password, method="sha256")
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for("auth.login"))
+        else:
+            return error, 418
+
+    return redirect(url_for("auth.login"))
+    # return render_template("register.html", title="Sign up")
+
+
+@app.route("/login", methods=("GET", "POST"))
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        error = None
+        remember = True if request.form.get("remember") else False
+        user = UserModel.query.filter_by(username=username).first()
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user.password, password):
+            error = "Incorrect password."
+
+        if error is None:
+            login_user(user, remember=remember)
+            return redirect(url_for("main.index"))
+        else:
+            flash("Please check your login details and try again.")
+            return redirect(url_for("auth.login"))
+
+    return redirect(url_for("main.index"))
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    # remove the username from the session if it's there
+    logout_user()
+    return redirect(url_for("main.index"))

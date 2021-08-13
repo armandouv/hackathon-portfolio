6994e7e7ba2d36522ce3a99ed2ec36b61ffcbb9e
Armando Ugalde <armauv@gmail.com>
2021-08-12 21:00:39 -0500

    Fix linting issues

--- app/__init__.py
+++ app/__init__.py
@@ -85,7 +85,8 @@ posts_base_url = base_url + "/posts/"
 
 @login_manager.user_loader
 def load_user(user_id):
-    # since the user_id is just the primary key of our user table, use it in the query for the user
+    # since the user_id is just the primary key of our user table,
+    # use it in the query for the user
     return UserModel.query.get(int(user_id))
 
 
